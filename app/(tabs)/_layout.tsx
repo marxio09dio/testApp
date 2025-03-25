cb489a5be2f141b45357758d2b3d4558388cc1a1
Márcio Araújo <38763806+marxio09dio@users.noreply.github.com>
2025-03-25 13:27:06 +0000

    change bottom

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,20 @@
 import { Tabs } from "expo-router"
 import React from "react"
-import { Platform } from "react-native"
-
-import { HapticTab } from "@/components/HapticTab"
+import { Platform, Pressable } from "react-native"
 import { IconSymbol } from "@/components/ui/IconSymbol"
 import TabBarBackground from "@/components/ui/TabBarBackground"
-import { Colors } from "@/constants/Colors"
-import { useColorScheme } from "@/hooks/useColorScheme"
+import { FontAwesome, SimpleLineIcons } from "@expo/vector-icons"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
+        tabBarHideOnKeyboard: true,
+        tabBarButton: (props) => <Pressable {...props} android_ripple={{ color: "transparent" }} />,
         tabBarBackground: TabBarBackground,
+        tabBarActiveTintColor: "#FF7A9D",
+        tabBarInactiveTintColor: "#868E96",
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -38,14 +35,14 @@ export default function TabLayout() {
         name="agenda"
         options={{
           title: "agenda",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar.badge.checkmark" color={color} />
+          tabBarIcon: ({ color, size }) => <SimpleLineIcons name="calendar" size={size} color={color} />
         }}
       />
       <Tabs.Screen
         name="pets"
         options={{
           title: "pets",
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="pawprint.fill" color={color} />
+          tabBarIcon: ({ color, size }) => <FontAwesome name="paw" size={size} color={color} />
         }}
       />
     </Tabs>
